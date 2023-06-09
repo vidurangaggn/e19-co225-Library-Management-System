54d9e49260c4a23a5421e6182dca876a6b57d454
nipulviduranga <71004802+nipulviduranga@users.noreply.github.com>
2023-06-09 14:59:00 +0530

    update login reg

--- app/src/main/java/com/example/lmsapplication/MainActivity.java
+++ app/src/main/java/com/example/lmsapplication/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.lmsapplication;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import android.os.Bundle;
 
 =======
 import android.content.Intent;
+=======
+>>>>>>> parent of f1c49b7 (Update login and Update)
 import android.os.Bundle;
-import android.widget.Toast;
 
+<<<<<<< HEAD
 import com.example.lmsapplication.LoginAndRegster.LoginActivity;
 import com.example.lmsapplication.databinding.ActivityMainBinding;
 >>>>>>> main
+=======
+>>>>>>> parent of f1c49b7 (Update login and Update)
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,10 +25,14 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import com.example.lmsapplication.databinding.ActivityMainBinding;
 =======
 
 >>>>>>> main
+=======
+import com.example.lmsapplication.databinding.ActivityMainBinding;
+>>>>>>> parent of f1c49b7 (Update login and Update)
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+<<<<<<< HEAD
 <<<<<<< HEAD
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -78,6 +88,20 @@ public class MainActivity extends AppCompatActivity {
         }
 
 >>>>>>> main
+=======
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home,R.id.issued_books,R.id.requested_books,R.id.requested_books,R.id.staff,R.id.my_account)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+>>>>>>> parent of f1c49b7 (Update login and Update)
     }
 
 }
